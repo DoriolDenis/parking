35f1dc04838fcec8e388c847d2e8070b1cd13d01
Denis FERRER <ferrerd@BTS-SN.LRQ>
2018-11-07 17:58:15 +0100

    Ceci est le début du programme pour afficher le message sur l'afficheur de la barriere.

---
+++ projetParking/projetParking.ino
@@ -0,0 +1,9 @@
+void setup() {
+  //Il faut faire un programme qui affiche une message qui demande de saisir le code sur l'afficheur lorsque une voiture est detecter 
+  //sur la boucle amont
+  
+}
+
+void loop() {
+
+}
