8e1a1477bc441df6d66ce192dffd76b93078b912
Denis FERRER <ferrerd@BTS-SN.LRQ>
2018-11-13 15:20:25 +0100

    reprise du programme principale pour ouvrir la barriere

--- prog_acces_parking/prog_acces_parking.ino
+++ prog_acces_parking/prog_acces_parking.ino
@@ -21,15 +21,24 @@ int tempo = 0, testTempo;         /// tempo permettra d'attendre les 30 secondes
 static int nombreVoiture = 0;     /// Variable static permettant de compter le nombre de vehicule dans le parking 
 
 ///Serial.println("Salut beaute");
-
+Wire.requestFrom(MECANISME,1);
+  while(Wire.available()){
+    int i;
+    i=Wire.read();
+    Serial.println(i);
+  }
 /******* Gestion de l'entree d'un vehicule *******/
 
-if (digitalRead(BOUCLEAMONT) == 166 && digitalRead(BOUCLEAVAL) == 230){     /// arrivee d'une voiture en entre du parking
-  if(digitalRead(VALIDATION) == 1) {                                    /// Si le code est valide
+if (digitalRead(BOUCLEAMONT) == 198 && digitalRead(BOUCLEAVAL) == 230){     /// arrivee d'une voiture en entre du parking
+  ///if(digitalRead(VALIDATION) == 1) {   /// Si le code est valide
+  
+  /*Wire.beginTransmission(0x20);
+  Wire.write(0xFD);   /// Ouvrir
+  Wire.endTransmission(MECANISME); */ 
+  commandeBarriere(OUVRIR,2);                                 
     testTempo = 1;
-    tempo;
-    commandeBarriere(OUVRIR,2);                                         /// Ouverture de la barriere 
-    while (digitalRead(BOUCLEAMONT) == 166 && digitalRead(BOUCLEAVAL) == 230 && testTempo == 1)
+    tempo;                                                                /// Ouverture de la barriere 
+    while (digitalRead(BOUCLEAMONT) == 198 && digitalRead(BOUCLEAVAL) == 230 && testTempo == 1)
     {
       delay(30);
       tempo++;                          /// Incrementation toute les 30ms pour simuler l'attentede 30 secondes 
@@ -37,35 +46,49 @@ if (digitalRead(BOUCLEAMONT) == 166 && digitalRead(BOUCLEAVAL) == 230){     /// 
         testTempo = 0;                  /// Le delay de 30 secondes est atteint 
       }
     }
+  
     delay(20);                          /// Evite une mauvaise lecture du if suivant si jamais il y a un rebond de l'inter bav
     if (digitalRead(BOUCLEAMONT) == 230 || digitalRead(BOUCLEAVAL) == 198){
-      if(digitalRead(BOUCLEAMONT) == 166){
-        while (digitalRead(BOUCLEAMONT) == 166 || digitalRead(BOUCLEAVAL) == 198);          /// On attend que le vehicule quitte les boucles
+      if(digitalRead(BOUCLEAMONT) == 198){
+        while (digitalRead(BOUCLEAMONT) == 198 || digitalRead(BOUCLEAVAL) == 198);          /// On attend que le vehicule quitte les boucles
         nombreVoiture++;
         Serial.println("Il y a dans le parking : ");
         Serial.println(nombreVoiture);
         Serial.println(" voiture(s)");
       }
     }
-    commandeBarriere(FERMER,2);
+ /* Wire.beginTransmission(0x20);
+  Wire.write(0xFE);   /// Fermer
+  Wire.endTransmission(MECANISME);*/
+  commandeBarriere(FERMER,2);
+  
   }
-}
+///}
 
 /******* Gestion de la sortie d'un vehicule *******/
 
 /// On ne decremente que si la voiture traverse les deux deux boucles
-if (digitalRead(BOUCLEAVAL) == 198 && digitalRead(BOUCLEAMONT) == 230){
-  Serial.println("Une voiture veut sortir");
+if (digitalRead(BOUCLEAVAL) == 166 && digitalRead(BOUCLEAMONT) == 230){
+ /// Serial.println("Une voiture veut sortir");
+  
+   /*Wire.beginTransmission(0x20);
+  Wire.write(0xFD);   /// Ouvrir
+  Wire.endTransmission(MECANISME);*/
   commandeBarriere(OUVRIR, 2);
-  while (digitalRead(BOUCLEAVAL) == 198 && digitalRead(BOUCLEAMONT) == 230);            /// On attend que le vehicule bouge
-  if(digitalRead(BOUCLEAVAL) == 198 || digitalRead(BOUCLEAMONT) == 166){
-    Serial.println("Une voiture sur une des deux boucles");
-    while (digitalRead(BOUCLEAVAL) == 198 || digitalRead(BOUCLEAMONT) == 166);          /// On attend que le vehicule soit parti
+  
+  while (digitalRead(BOUCLEAVAL) == 166 && digitalRead(BOUCLEAMONT) == 230);            /// On attend que le vehicule bouge
+  if(digitalRead(BOUCLEAVAL) == 166 || digitalRead(BOUCLEAMONT) == 198){
+    ///Serial.println("Une voiture sur une des deux boucles");
+    while (digitalRead(BOUCLEAVAL) == 166 || digitalRead(BOUCLEAMONT) == 198);          /// On attend que le vehicule soit parti
     nombreVoiture --;
     Serial.println("Il y a le parking : ");
     Serial.println(nombreVoiture);
     Serial.println(" voiture(s)");
   }
+  
+   /*Wire.beginTransmission(0x20);
+  Wire.write(0xFE);   /// Fermer
+  Wire.endTransmission(MECANISME);*/
   commandeBarriere(FERMER, 2);
 }
 /// Fin du programme de sortie d'un vehicule
