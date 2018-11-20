1d2c5ecac3af0f3788371482c1c2602fa860f86f
Denis FERRER <ferrerd@BTS-SN.LRQ>
2018-11-20 14:41:40 +0100

    changement complet du code d'ouverture de la barrière

--- prog_acces_parking/fonction.cpp
+++ prog_acces_parking/fonction.cpp
@@ -1,88 +1,37 @@
-#include "fonction.h"       /// Permet de recuperer le prototype des fonctions ainsi que les differentes constantes
-#include "arduino.h"        /// a ne mettre que si l'on utilise un fichier separee
+#include "fonction.h"       // Permet de recuperer le prototype des fonctions ainsi que les differentes constantes
+#include "arduino.h"        // a ne mettre que si l'on utilise un fichier separee
 #include <Wire.h>
 
-void commandeBarriere(int sens, int tours)
+void ouvrir()
 {
-  pinMode (PAS1,OUTPUT);    /// Initialisation des broches en sortie
-  pinMode (PAS2,OUTPUT);
-  pinMode (PAS3,OUTPUT);
-  pinMode (PAS4,OUTPUT);
-    
-    if (sens)       /// Suivant la valeur du sens, on ouvre ou on ferme la barriere
-    {
-      ouvrir(tours);
-    }
-    else
-    {
-      fermer(tours);
-    }
+    Wire.beginTransmission(0x20);
+    Wire.write(0xFD);
+    Wire.endTransmission();
 }
 
-void ouvrir(int tours)
+void fermer()
 {
-  int i,j;
-    for(i=0; i<tours; i++)
-    {
-      for(j=0;j<12;j++)       /// On repete la sequence de 4 douze fois pour realiser 1 tours (moteur 48 pas)
-      {
-        seq(1);
-        delay(TEMPO);         /// Temporisation pour tenir compte du cahier des charges
-        seq(2);
-        delay(TEMPO);
-        seq(3);
-        delay(TEMPO);
-        seq(4);
-        delay(TEMPO);
-      }
-    }
+    Wire.beginTransmission(0x20);
+    Wire.write(0xFE);
+    Wire.endTransmission();
 }
 
-void fermer(int tours)
+
+unsigned char lectureBam()
 {
-  int i,j;
-    for(i=0; i<tours; i++)
-    {
-      for(j=0;j<12;j++)       /// On repete la sequence de 4 douze fois pour realiser 1 tours (moteur 48 pas)
-      {
-        seq(4);
-        delay(TEMPO);         /// Temporisation pour tenir compte du cahier des charges
-        seq(3);
-        delay(TEMPO);
-        seq(2);
-        delay(TEMPO);
-        seq(1);
-        delay(TEMPO);
-      }
-    }
+  Wire.requestFrom(0x20, 1);
+  unsigned char a = Wire.read();
+  
+  Serial.println(a, DEC);
+  return a;
+
 }
 
-void seq(int numeroSeq)       /// Fonction qui permet de generer les 4 pas
+unsigned char lectureBav()
 {
-  switch (numeroSeq)
-  {
-    case 1 : digitalWrite(PAS1,HIGH);
-             digitalWrite(PAS2,LOW);
-             digitalWrite(PAS3,LOW);
-             digitalWrite(PAS4,HIGH);
-             break;
-             
-   case 2 :  digitalWrite(PAS1,LOW);
-             digitalWrite(PAS2,HIGH);
-             digitalWrite(PAS3,LOW);
-             digitalWrite(PAS4,HIGH);
-             break;
-             
-   case 3 :  digitalWrite(PAS1,LOW);
-             digitalWrite(PAS2,HIGH);
-             digitalWrite(PAS3,LOW);
-             digitalWrite(PAS4,HIGH);
-             break;
-             
-  case 4 :   digitalWrite(PAS1,HIGH);
-             digitalWrite(PAS2,LOW);
-             digitalWrite(PAS3,HIGH);
-             digitalWrite(PAS4,LOW);
-             break;    
-  }
+  Wire.requestFrom(0x20, 1);
+  unsigned char a = Wire.read();
+  
+  Serial.println(a, DEC);
+  return a;
 }
